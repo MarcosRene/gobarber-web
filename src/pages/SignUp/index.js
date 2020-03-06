d882d6f6506e31d879e501bc554bb19500b3567a
Marcos Renê Cavalcante Moura <marcosrene@alu.ufc.br>
2020-03-06 11:14:20 -0300

    0.5.0
    #estilização (dashboard, register) e adicionando unform

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-export default function SignUp() {
-  return <div />;
+export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
